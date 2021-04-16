29fd386b591b88b475060892995e7163a73a2289
西澤璃音 <nishizawa-rion@webrage.jp>
2021-04-16 17:26:12 +0900

    update js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
   <p>Reactの、GitHub Page公開テストです。</p>,
   document.getElementById('root')
 );
